19a41aa11cad3b1be701a76bc67623f3d7bcb5f3
mx.voloshin <mx.voloshin@gmail.com>
2015-04-10 08:31:49 +0200

    test

--- Banalyzer.Domain/Common/Deposite.cs
+++ Banalyzer.Domain/Common/Deposite.cs
@@ -1,7 +1,9 @@
-﻿namespace Banalyzer.Domain.Common
+﻿using System;
+
+namespace Banalyzer.Domain.Common
 {
     public class Deposite
     {
-         
+        public DateTime OpenedDate { get; set; }
     }
 }
